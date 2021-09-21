3ecd558a133877bc75333beac506801522ecfe1c
santorsm <matt.santorsola@gmail.com>
2021-09-21 00:18:37 -0700

    matts solution

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,24 @@
-function bottleSong() {
-  // Write your code here!
-};
+const bottleSong = (num) => {
 
-bottleSong();
+  //variable to hold current # of bottles
+  let numBottles = num;
+
+  while (numBottles > 1){
+    console.log(`${numBottles} bottle${numBottles > 1 ? 's' : ''} of beer on the wall, ${numBottles} bottles of beer.`)
+    console.log(`Take one down and pass it around, ${numBottles - 1} bottle${numBottles - 1 > 1 ? 's' : ''} of beer on the wall.`)
+    numBottles--
+
+  }
+  console.log("1 bottle of beer on the wall, 1 bottle of beer.")
+  console.log("Take one down and pass it around, no more bottles of beer on the wall.")
+  console.log("No more bottles of beer on the wall, no more bottles of beer.")
+  console.log(`Go to the store and buy some more, ${num} bottles of beer on the wall.`)
+
+}
+
+
+// function bottleSong(n=5) {
+
+// };
+
+bottleSong(5);
